e5289ad7966409f9dcf3eb00a09b708616083a85
Nick Castagnaro <nickshs123@gmail.com>
2021-08-25 23:13:10 -0400

    Update array.js

--- array.js
+++ array.js
@@ -1,5 +1,5 @@
 //This program takes numbers, converts them to strings, so that way they can output the number+ the next number for each index of the array
-//Example: 203421 should output [2,20,203,2034,20342,203421]..
+//Example: 203421 should output [2,20,203,2034,20342,203421]... 
 
 function createArrayOfTiers(num) {      
     let x = num.toString();             //Converts the number to a string, so it is able to be worked on.
